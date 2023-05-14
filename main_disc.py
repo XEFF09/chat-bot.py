4f1d6c70a5876411c39f17e4e0d9f6ae259265fe
ppu.nn <92360888+XEFF09@users.noreply.github.com>
2023-05-14 23:17:29 +0700

    global sync, unsync added

--- main_disc.py
+++ main_disc.py
@@ -1,3 +1,4 @@
+from typing import Optional, Literal
 from discord import Intents
 from discord.ext import commands as cmds
 from dotenv import load_dotenv
@@ -23,5 +24,16 @@ async def main():
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now ready!')
+
+@bot.command()
+@cmds.is_owner()
+async def sync(ctx: cmds.Context, spec: Optional[Literal['add', 'rem']] = None) -> None:
+    if spec == 'add':
+        fmt = await ctx.bot.tree.sync()
+        await ctx.send(f'total synced: {len(fmt)}')
+    elif spec == 'rem':
+        ctx.bot.tree.clear_commands(guild=None)
+        await ctx.bot.tree.sync(guild=None)
+        await ctx.send(f'total unsynced: all')
 
 asyncio.run(main())
